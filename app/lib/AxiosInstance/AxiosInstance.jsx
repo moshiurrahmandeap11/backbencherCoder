913f190077d2845c9a9d6427b6d2f21be5b987aa
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-01-20 00:27:32 +0600

    updated

--- app/lib/AxiosInstance/AxiosInstance.jsx
+++ app/lib/AxiosInstance/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://server-backbencher-coder.onrender.com/api",
   timeout: 10000, // 10s
   headers: {
     "Content-Type": "application/json",
